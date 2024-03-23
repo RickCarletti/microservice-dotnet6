6cc854dfacf3fd8b7448ec596058c2585b556adf
RickCarletti <ricardocarletti@id.uff.br>
2024-03-23 19:50:30 -0300

    fixing configs of projects

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemyPerson/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemyPerson/Model/Person.cs
@@ -0,0 +1,6 @@
+﻿namespace RestWithASPNETUdemy-person.Model
+{
+    public class Person
+{
+}
+}
