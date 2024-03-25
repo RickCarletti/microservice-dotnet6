f56a7039ca35bf708cdf1e203195b981b372f3ce
RickCarletti <ricardocarletti@id.uff.br>
2024-03-25 13:54:03 -0300

    [NEW] finalizing the complete product CRUD

---
+++ dotNet6 - Part 1/GeekShooping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/Product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _productRepository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO product)
+        {
+            if (product == null) { return BadRequest(); }
+
+            product = await _productRepository.Create(product);
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO product)
+        {
+            if (product == null) { return BadRequest(); }
+
+            await _productRepository.Update(product);
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var excluido = await _productRepository.Delete(id);
+            if (!excluido) return BadRequest();
+            return Ok(excluido);
+        }
+    }
+}
